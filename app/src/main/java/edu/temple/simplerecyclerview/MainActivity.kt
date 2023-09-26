0c6d9fad6b84eacbfb25d1662b083efdf5dbada5
Karl Morris <karl.morris@temple.edu>
2023-09-26 09:40:23 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -0,0 +1,20 @@
+package edu.temple.simplerecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Step 1: Reference RecyclerView object
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        //Step 2: Provide a LayoutManager
+
+
+        //Step 4: Provide a RecyclerView.Adapter
+    }
+}
---
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -0,0 +1,14 @@
+package edu.temple.simplerecyclerview
+
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+// Step 3: Complete adapter implementation
+
+class NumberDisplayAdapter /* Step 3a: Provide Adapter Parent */ {
+
+    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
+
+    //Step 3b: Complete function definitions for adapter
+
+}
